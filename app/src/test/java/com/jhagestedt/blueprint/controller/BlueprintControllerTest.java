200476e5e66d6fa909533e404ce613d2ae898e8a
jhagestedt <julien.hagestedt@gmail.com>
2020-05-23 21:02:50 +0200

    feat: security

--- app/src/test/java/com/jhagestedt/blueprint/controller/BlueprintControllerTest.java
+++ app/src/test/java/com/jhagestedt/blueprint/controller/BlueprintControllerTest.java
@@ -1,6 +1,8 @@
 package com.jhagestedt.blueprint.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jhagestedt.blueprint.BlueprintApplication;
+import com.jhagestedt.blueprint.BlueprintRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,14 +24,28 @@ public class BlueprintControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Test
+  public void shouldReturnCreated() throws Exception {
+    String content = objectMapper.writeValueAsString(new BlueprintRequest()
+      .setData("data"));
+    mockMvc.perform(MockMvcRequestBuilders
+      .post("/api/v1/blueprint")
+      .accept(MediaType.APPLICATION_JSON_VALUE)
+      .contentType(MediaType.APPLICATION_JSON_VALUE)
+      .content(content))
+      .andDo(MockMvcResultHandlers.print())
+      .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
 
   @Test
   public void shouldReturnNotFound() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders
       .get("/api/v1/blueprint/abc")
       .accept(MediaType.APPLICATION_JSON_VALUE)
-      .contentType(MediaType.APPLICATION_JSON_VALUE)
-    )
+      .contentType(MediaType.APPLICATION_JSON_VALUE))
       .andDo(MockMvcResultHandlers.print())
       .andExpect(MockMvcResultMatchers.status().isNotFound());
   }
@@ -37,15 +53,13 @@ public class BlueprintControllerTest {
   @Test
   public void shouldReturnUnsupportedMediaType() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders
-      .get("/api/v1/blueprint/abc")
-    )
+      .get("/api/v1/blueprint/abc"))
       .andDo(MockMvcResultHandlers.print())
       .andExpect(MockMvcResultMatchers.status().isUnsupportedMediaType());
     mockMvc.perform(MockMvcRequestBuilders
       .get("/api/v1/blueprint/abc")
       .accept(MediaType.APPLICATION_ATOM_XML_VALUE)
-      .contentType(MediaType.APPLICATION_ATOM_XML_VALUE)
-    )
+      .contentType(MediaType.APPLICATION_ATOM_XML_VALUE))
       .andDo(MockMvcResultHandlers.print())
       .andExpect(MockMvcResultMatchers.status().isUnsupportedMediaType());
   }
