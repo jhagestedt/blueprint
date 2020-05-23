a28bce6af21191a95e1ef439c5e1e40d5a46c20b
Julien Hagestedt <julien.hagestedt@gmail.com>
2020-05-23 21:05:34 +0200

    feat: security (#6)

--- app/src/main/java/com/jhagestedt/blueprint/config/SecurityConfig.java
+++ app/src/main/java/com/jhagestedt/blueprint/config/SecurityConfig.java
@@ -1,7 +1,36 @@
 package com.jhagestedt.blueprint.config;
 
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  protected HttpFirewall strictFirewall() {
+    StrictHttpFirewall firewall = new StrictHttpFirewall();
+    firewall.setAllowedHttpMethods(Arrays.asList(
+      HttpMethod.GET.name(),
+      HttpMethod.POST.name()
+    ));
+    return firewall;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .mvcMatchers("/api/**").permitAll()
+      .mvcMatchers("/management/*").permitAll()
+      .anyRequest().denyAll()
+      .and().csrf().disable();
+  }
+
 }
