bc63b61436759b579d8bf137f9c7370dfb10cd2b
Julien Hagestedt <julien.hagestedt@gmail.com>
2020-05-23 20:33:37 +0200

    feat: actuator (#5)

--- app/src/main/java/com/jhagestedt/blueprint/config/OpenApiConfig.java
+++ app/src/main/java/com/jhagestedt/blueprint/config/OpenApiConfig.java
@@ -3,17 +3,16 @@ package com.jhagestedt.blueprint.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-  @Value("${spring.application.name}")
-  private String name;
-  @Value("${spring.application.version}")
-  private String version;
+  @Autowired
+  private BuildProperties buildProperties;
 
   /**
    * Configure the OpenApi bean.
@@ -24,8 +23,8 @@ public class OpenApiConfig {
   public OpenAPI openApi() {
     return new OpenAPI()
       .info(new Info()
-        .title(name)
-        .version(version)
+        .title(buildProperties.getName())
+        .version(buildProperties.getVersion())
         .license(new License()
           .name("Apache 2.0")
           .url("http://www.apache.org/licenses/LICENSE-2.0")));
